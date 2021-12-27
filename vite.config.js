257073301d013fa819efc4ff0d57a9b94278aa4a
Guillaume Briday <guillaumebriday@gmail.com>
2021-12-28 00:07:03 +0100

    Fixing Vite config for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-const path = require('path')
+import path from 'path'
 
-module.exports = {
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'stimulus-notification'
-    },
-    rollupOptions: {
-      external: ['stimulus-use', '@hotwired/stimulus'],
-      output: {
-        globals: {
-          '@hotwired/stimulus': 'Stimulus',
-          'stimulus-use': 'useTransition'
+export default ({ mode }) => {
+  if (mode === 'netlify') return {}
+
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/index.ts'),
+        name: 'stimulus-notification'
+      },
+      rollupOptions: {
+        external: ['stimulus-use', '@hotwired/stimulus'],
+        output: {
+          globals: {
+            '@hotwired/stimulus': 'Stimulus',
+            'stimulus-use': 'useTransition'
+          }
         }
       }
     }
